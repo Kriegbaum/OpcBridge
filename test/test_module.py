f7103447cc7f32249dd7fa9a999088201263632f
Spidergod <spid3rbox@gmail.com>
2021-01-17 15:18:15 -0800

    Fix typo in test

--- test/test_module.py
+++ test/test_module.py
@@ -5,8 +5,8 @@ def test_makeEightBit():
 def test_rgbSetBrightness():
     assert rgbSetBrightness(255, [1,1,1]) == [255,255,255]
     assert rgbSetBrightness(255, [0,0,0]) == [0,0,0]
-    assert rgbSetBrightness(0, [255,255,255]) == 0
+    assert rgbSetBrightness(0, [255,255,255]) == [0,0,0]
 
 def test_brightnessChange():
     assert brightnessChange([128,128,128], 0) == [128,128,128]
-    assert brightnessChange([128,128,128], 255) = [255,255,255]
+    assert brightnessChange([128,128,128], 255) == [255,255,255]
